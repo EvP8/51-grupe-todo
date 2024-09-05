ebbb984e40c9bfdcaffa571095785ed64245f53e
Evaldas Petkus <evaldas.db@gmail.com>
2024-09-05 09:27:11 +0300

    Lightbox events

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
-console.log('Labas rytas Lietuva!');
+// lightbox
+const newTaskBtnDOM = document.querySelector('.main-header .btn');
+const lightboxDOM = document.querySelector('.lightbox');
+const lightboxCloseDOM = lightboxDOM.querySelector('.close');
+const lightboxBackgroundDOM = lightboxDOM.querySelector('.background');
+
+newTaskBtnDOM.addEventListener('click', () => {
+    lightboxDOM.dataset.visible = 'true';
+});
+
+lightboxCloseDOM.addEventListener('click', () => {
+    lightboxDOM.dataset.visible = 'false';
+});
+
+lightboxBackgroundDOM.addEventListener('click', () => {
+    lightboxDOM.dataset.visible = 'false';
+});
+
+window.addEventListener('keyup', e => {
+    if (e.key === 'Escape') {
+        lightboxDOM.dataset.visible = 'false';
+    }
+});
+
+// task menu
+const allTaskDOMs = document.querySelectorAll('.task');
+
+for (const taskDOM of allTaskDOMs) {
+    const moreDOM = taskDOM.querySelector('.more');
+    const moreActionsDOM = taskDOM.querySelector('.more-actions');
+
+    if (moreDOM) {
+        moreDOM.addEventListener('click', () => {
+            moreActionsDOM.dataset.visible = 'true';
+        });
+    }
+}
+
+document.body.addEventListener('click', () => {
+    console.log('window click...');
+
+    for (const taskDOM of allTaskDOMs) {
+        taskDOM.querySelector('.more-actions').dataset.visible = 'false';
+    }
+});
